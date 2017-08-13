15582ca1185a4c4ae8bfc0ffbc45f17fa07bd9da
andrewjc88 <andrewjc88@gmail.com>
2017-08-13 15:04:33 -0700

    Added clear marker and clear old listings on reload

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,22 @@
-'use strict';
+// 'use strict';
 
 $(document).foundation();
 
 function ViewModel(){
 
   this.zipInput = ko.observable(''); // Zipcode input field 
-  this.gDeal = ko.observableArray([]); // inital deals array
-
+  this.gDeals = ko.observableArray([]); // inital deals array
 
   var self = this;
   var markers = [];
   var polygon = null;
   var map;
-  var initLoc = {lat: 37.7749, lng: -122.4194};
+  var initLoc = {lat: 37.7749, lng: -122.4194}; // SF Baby!
+  
+  var iconImg = {
+    url: '/dist/img/gIcon_2.png',
+    scaledSize: new google.maps.Size(40, 40)
+  };
 
   this.newLoc = function() {
     var currentLoc = this.zipInput();
@@ -29,16 +33,23 @@ function ViewModel(){
       geocoder.geocode(
         { address: address },
         function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
+          if (status == google.maps.GeocoderStatus.OK) {       
+
+            // Lets make an object to pass into groupon request
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+            var latLng = {lat: lat, lng: lng};
+            // Set map to new location
             map.setCenter(results[0].geometry.location);
-            map.setZoom(14);
+            map.setZoom(13);
+            // Send ajax request latlngs
+            getGDeals(latLng);
           } else {
             window.alert('We could not find that location - try entering a more specifif place.');
           }
         }
       );
     }
-    grouponDeals(initLoc);
   };
 
   function initMap() {
@@ -128,85 +139,108 @@ function ViewModel(){
 
     map = new google.maps.Map(document.getElementById('map'), {
       center: initLoc,
-      zoom: 15,
+      zoom: 13,
       styles: styles,
       mapTypeControl: false,
         
     });
-    grouponDeals(initLoc);
+    getGDeals(initLoc);
   }
 
-  function grouponDeals(location) {
-    var gUrl = "https://partner-api.groupon.com/deals.json?tsToken=IE_AFF_0_200012_212556_0&filters=category:food-and-drink&limit=30&offset=0&division_id=san-francisco";
+  function getGDeals(location) {
+    var gUrl = "https://partner-api.groupon.com/deals.json?tsToken=IE_AFF_0_200012_212556_0&filters=category:food-and-drink&limit=30&offset=0&";
     var loc = location;
-
-    // Format location for groupon
     
-    console.log(location);
     $.ajax({
-      url: gUrl,
+      url: gUrl + 'lat=' + loc.lat + '&lng=' + loc.lng,
       dataType: 'jsonp',
       success: function(data) {
-        console.log(data);
+        // console.log(data);
+        var len = data.deals.length;
+        for (var i = 0; i < len; i++) {
+
+          var dealLoc = data.deals[i].options[0].redemptionLocations[0];
+          // Filter out deals without address
+          if (data.deals[i].options[0].redemptionLocations[0] === undefined) continue;
+          
+          var name = data.deals[i].merchant.name;
+            lat = dealLoc.lat,
+            lng = dealLoc.lng,
+            url = data.deals[i].dealUrl,
+            img = data.deals[i].mediumImageUrl,
+            about = data.deals[i].pitchHtml,
+            address = dealLoc.streetAddress1,
+            city = dealLoc.city,
+            state = dealLoc.state,
+            zip = dealLoc.postalCode,
+            shortAbout = data.deals[i].announcementTitle,
+            tags = data.deals[i].tags;
+          
+          self.gDeals.push({
+            name: name,
+            lat: lat,
+            lng: lng,
+            url: url,
+            img: img,
+            about: about,
+            address: address + "br" + city + ", " + state + " " + zip,
+            shortAbout: shortAbout,
+            tags: tags
+          });
+        }
+        makeMarkers(self.gDeals());
+        
       }
-      //   var len = data.deals.length;
-      //   for(var i = 0; i < len; i++) {
-      //     var gLoc = data.deals[i].options[0].redemptionLocation[0];
-
-      //     if (data.deals[i].options[0].redemptionLocation[0] === undefined) continue;
-
-      //     var venue = data.deals[i].merchant.name;
-
-      //   }
-      // }
     });
   }
 
-  function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21,34));
-    return markerImage;
+
+  function makeMarkers(deals) {
+    //console.log(deals);
+    var largeInfowindow = new google.maps.InfoWindow();    
+    
+    var len = deals.length;
+    for (i = 0; i < len; i++) {
+      
+      var gMarker = new google.maps.Marker({
+        position: {lat: deals[i].lat, lng: deals[i].lng},
+        map: map,
+        icon: iconImg,
+        animation: google.maps.Animation.DROP,
+        title: deals[i].name,
+        content: deals[i]
+      });
+
+      markers.push(gMarker);
+
+      gMarker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+        // console.log(this);
+      });
+    }
   }
 
-  // var defaultIcon = makeMarkerIcon('0091ff');
-  // var highlightedIcon = makeMarkerIcon('ffff24');
+  function populateInfoWindow(marker, infowindow) {
 
-  // for (var i = 0; i < locations.length; i++) {
-  //     // Get position from current array.
-  //     var position = locations[i].location;
-  //     var title = location[i].title;
-  //
-  //     var marker = new google.maps.Marker({
-  //         position: position,
-  //         title: title,
-  //         animation: google.maps.Animation.DROP,
-  //         icon: defaultIcon,
-  //         id: i
-  //     });
-  // };
-  //
-  // function showListings() {
-  //     var bounds = new google.maps.LatLngBounds();
-  //     for (var i = 0; i < markers.length; i++) {
-  //         markers[i].setMap(map);
-  //         bounds.extend(markers[i].position);
-  //     }
-  //     map.fitBounds(bounds);
-  // };
-  //
-  // function hideListings() {
-  //     for (var i = 0; i < markers.length; i++) {
-  //         markers[i].setMap(null);
-  //     }
-  // };
+    var infowindowData =
+    '<div id="infowindow">' +
+    '<img src="' + marker.content.img + '">' +
+    '<h4>' + marker.content.shortAbout + '</h4>' +
+    '<p>' + marker.content.address + '</p>' +
+    '<p><a href="' + marker.content.url + '" target="_blank">View deal</a></p>' +
+    '<p>' + marker.content.about + '</p></div>';
 
-
-
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent(infowindowData);
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
+  }
 
   var drawingManager = new google.maps.drawing.DrawingManager();
   drawingManager.setMap(map);
@@ -223,10 +257,8 @@ function ViewModel(){
           drawingManager.setMap(map);
       }
   }
-
   
   initMap();
-
 
 }
 
